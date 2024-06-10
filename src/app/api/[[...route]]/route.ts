73dbc126e3c4b72774142d546ede189539afda8d
Mohammed <gsft22221@gmail.com>
2024-06-10 03:59:17 +0300

    feat(api): create api search endpoint with pagination

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,3 +1,7 @@
+import _terms from '@/data/terms.json'
+import { Term } from '@/types'
+import { paginate } from '@/utils/array'
+import Fuse from 'fuse.js'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
@@ -5,10 +9,44 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/', async (c, next) => {
-  return c.json({
-    message: 'Hello World',
-  })
+// cache every request for an hour
+app.use(async (c, next) => {
+  await next()
+  c.header('Cache-Control', 'max-age=3600')
+})
+
+// the amount of items per page
+const pageSize = 10
+
+// Fuse instance to be re-used in each request.
+// putting it outside of the request handler is better for performance
+const terms = new Fuse(_terms, {
+  keys: ['arabic', 'english'],
+  includeScore: false,
+  isCaseSensitive: false,
+  useExtendedSearch: true,
+  ignoreLocation: true,
+  ignoreFieldNorm: true,
+})
+
+app.get('/search', async (c, next) => {
+  let query = c.req.query('q') || ''
+  let pageNumber = parseInt(c.req.query('page') || '1', 10)
+
+  let searchResult: Term[] = []
+  let totalPages = 0
+
+  if (query) {
+    const fullSearchResult = terms.search(`'${query!}`)
+    totalPages = Math.ceil(fullSearchResult.length / pageSize)
+
+    searchResult = paginate(fullSearchResult, pageSize, pageNumber).map((r) => r.item)
+  } else {
+    totalPages = Math.ceil(_terms.length / pageSize)
+    searchResult = paginate(_terms, pageSize, pageNumber)
+  }
+
+  return c.json({ result: searchResult, totalPages })
 })
 
 export const GET = handle(app)
