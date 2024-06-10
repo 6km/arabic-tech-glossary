53e8b478e7bcde6afc274bcda2f4855be22d4a7b
Mohammed <gsft22221@gmail.com>
2024-06-10 04:04:59 +0300

    create types.ts

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+import { ColumnDef } from '@tanstack/react-table'
+
+export type Term = {
+  arabic: string
+  english: string
+  description?: string
+}
+
+export interface TableProps {
+  data: Term[]
+  columns: ColumnDef<unknown, any>[]
+}
