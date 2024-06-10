5deb5281c2116000e2977b0d6b93b2b0256d330c
Mohammed <gsft22221@gmail.com>
2024-06-10 04:03:45 +0300

    feat(components): create footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-between border-t border-t-[#D8DDE5] py-8">
+      <h3 className="font-bold">
+        قال رسول الله ﷺ &rdquo;من سَلَكَ طريقاً يلتمس به علماً؛ سهل الله به طريقاً إلى الجنة&ldquo;
+      </h3>
+
+      <Link
+        target="_blank"
+        href="https://github.com/6km/techionary"
+        className="font-medium text-[#0C87FA] hover:underline"
+      >
+        تحرير الصفحة على GitHub
+      </Link>
+    </footer>
+  )
+}
