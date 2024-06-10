98a2506b0e10d72211d7f456a929274a47032790
Mohammed <gsft22221@gmail.com>
2024-06-10 04:02:31 +0300

    feat(components): create loading screen

---
+++ src/components/screens/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex h-48 flex-col items-center justify-center gap-2 border border-[#D7DCE2] bg-[#E6EAEF] text-center">
+      <h3 className="text-xl font-bold">جاري التحميل</h3>
+      <p>يتم تحميل البيانات المطلوبة</p>
+    </div>
+  )
+}
